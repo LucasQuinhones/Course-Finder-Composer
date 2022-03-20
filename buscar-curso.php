4ec75daedf01237c1060b1f59e6abdd1508e3ecf
Gian Alves <gian.alves@jrcomercio.com.br>
2022-03-20 18:56:01 -0300

    Primeiro Commit

--- buscar-curso.php
+++ buscar-curso.php
@@ -1,7 +1,17 @@
 <?php
 
-use GuzzleHttp\Client;
+require 'vendor/autoload.php';
 
-$client = new Client(['verify' => false]);
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-$html = $resposta->getBody();
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false, 'base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo exibeMensagem($curso);
+}
